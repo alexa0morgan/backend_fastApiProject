3ab5148ffbb46dff3d8992ec09310ae6c24ccb13
alexa0morgan <alexa0morgan@users.noreply.github.com>
2024-06-11 23:46:30 +0700

    refactor: создание сервиса для брендов

--- routers/brand_router.py
+++ routers/brand_router.py
@@ -1,75 +1,44 @@
-from datetime import datetime, UTC
+from fastapi import APIRouter, Depends
 
-from fastapi import APIRouter, HTTPException, Depends, status
-
-import services.brand_service as BrandService
-from db import CurrentSession
-from models.brand_model import Brand, BrandCreate, BrandResponse, BrandUpdate, BrandQuery
+from models.brand_model import BrandCreate, BrandResponse, BrandUpdate, BrandQuery
 from routers.auth_router import CurrentAdminUser
-from services.base_service import get_all
+from services.brand_service import BrandService
 
 router = APIRouter()
 
 
 @router.post("/create", response_model=BrandResponse)
 def create_brand(
-        session: CurrentSession,
         _: CurrentAdminUser,
         brand: BrandCreate,
+        brand_service: BrandService = Depends()
 ):
-    return BrandService.create_brand(session, brand)
+    return brand_service.create(brand)
 
 
 @router.get("/", response_model=list[BrandResponse])
 def read_brands(
-        session: CurrentSession,
         _: CurrentAdminUser,
-        query: BrandQuery = Depends()
-
+        query: BrandQuery = Depends(),
+        brand_service: BrandService = Depends()
 ):
-    options = [Brand.deleted_at == None]
-
-    if query.id:
-        options.append(Brand.id == query.id)
-    if query.id_in:
-        options.append(Brand.id.in_(query.id_in))
-    if query.name:
-        options.append(Brand.name.ilike(f'%{query.name}%'))
-    if query.name_in:
-        options.append(Brand.name.in_(query.name_in))
-
-    return get_all(session, Brand, options, query)
+    return brand_service.read(query)
 
 
 @router.patch("/update/{brand_id}", response_model=BrandResponse)
 def update_brand(
-        session: CurrentSession,
         _: CurrentAdminUser,
         brand_id: int,
         brand: BrandUpdate,
+        brand_service: BrandService = Depends()
 ):
-    db_brand = session.get(Brand, brand_id)
-    if not brand:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Brand not found")
-
-    brand_data = brand.model_dump(exclude_unset=True)  # exclude_unset=True - исключает неустановленные значения
-    db_brand.sqlmodel_update(brand_data)  # обновление значений
-    session.add(db_brand)
-    session.commit()
-    session.refresh(db_brand)
-    return db_brand
+    return brand_service.update(brand_id, brand)
 
 
 @router.delete("/delete/{brand_id}")
 def delete_brand(
-        session: CurrentSession,
         _: CurrentAdminUser,
         brand_id: int,
+        brand_service: BrandService = Depends()
 ):
-    db_brand = session.get(Brand, brand_id)
-    if not db_brand:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Brand not found")
-
-    db_brand.deleted_at = datetime.now(UTC).isoformat()
-    session.commit()
-    return {"message": "Brand deleted successfully"}
+    return brand_service.delete(brand_id)
--- services/brand_service.py
+++ services/brand_service.py
@@ -1,12 +1,38 @@
-from fastapi import Query
-from sqlmodel import Session
+from datetime import datetime, UTC
 
-from models.brand_model import Brand, BrandCreate
+from models.brand_model import Brand, BrandCreate, BrandQuery, BrandUpdate
+from services.base_service import BaseService
 
 
-def create_brand(session: Session, brand: BrandCreate) -> Brand:
-    db_brand = Brand.model_validate(brand)
-    session.add(db_brand)
-    session.commit()
-    session.refresh(db_brand)
-    return db_brand
+class BrandService(BaseService):
+
+    def create(self, brand: BrandCreate) -> Brand:
+        db_brand = Brand.model_validate(brand)
+        return self.save_and_refresh(db_brand)
+
+    def read(self, query: BrandQuery):
+        options = [Brand.deleted_at == None]
+
+        if query.id:
+            options.append(Brand.id == query.id)
+        if query.id_in:
+            options.append(Brand.id.in_(query.id_in))
+        if query.name:
+            options.append(Brand.name.ilike(f'%{query.name}%'))
+        if query.name_in:
+            options.append(Brand.name.in_(query.name_in))
+
+        return self.get_all(Brand, options, query)
+
+    def update(self, brand_id: int, brand: BrandUpdate) -> Brand:
+        db_brand = self.get_one(Brand, brand_id)
+        brand_data = brand.model_dump(exclude_unset=True)  # exclude_unset=True - исключает неустановленные значения
+        db_brand.sqlmodel_update(brand_data)
+        return self.save_and_refresh(db_brand)
+
+    def delete(self, brand_id: int):
+        db_brand = self.get_one(Brand, brand_id)
+
+        db_brand.deleted_at = datetime.now(UTC).isoformat()
+        self.session.commit()
+        return {"message": "Brand deleted successfully"}
